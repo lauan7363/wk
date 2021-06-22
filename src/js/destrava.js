8521727edff46fc49a27017870b6d859a0d4ff1e
Lauan <78829707+lauan7363@users.noreply.github.com>
2021-06-22 10:10:57 -0300

    Add files via upload

---
+++ src/js/destrava.js
@@ -0,0 +1,12 @@
+const destrava = () => {
+    return `⏰DESTRAV2.0⏰
+    li.DESTRAVip.to
+    2019 🦍💨
+    `
+}
+exports.destrava = destrava
+
+const destrava2 = () => {
+    return `👻❤DESTRAVA DARKAN❤👻
+`}
+exports.destrava2 = destrava2
